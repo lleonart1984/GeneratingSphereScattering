9589f7aa4cd95415bf1b75391f2ef336ed4d6ec7
Ludwig Leonard <lleonart1984@gmail.com>
2020-12-01 14:35:48 +0100

    initial commit

---
+++ SphereScattering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SphereScattering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
